9172d689297af0a842fc5fdae4fb5b591ea1810d
HHHanson <yuhanghu@umich.edu>
2025-11-24 01:33:32 +0800

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,77 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+video=this.document.querySelector("#player1");
+video.autoplay=false;
+console.log("Autoplay is set to "+video.autoplay);
+video.loop=false;
+console.log("Loop is set to "+video.loop);
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML=video.volume*100+"%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down Video");
+	video.playbackRate*=0.9;
+	console.log("Speed is "+video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up Video");
+	video.playbackRate/=0.9;
+	console.log("Speed is "+video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Skip Ahead Video");
+	if (video.currentTime+10<video.duration){
+		video.currentTime+=10;
+	}
+	else{
+		video.currentTime=0;
+	}
+	console.log("Current location is "+video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted==false){
+		video.muted=true;
+		document.querySelector("#mute").innerHTML="Unmute";
+		console.log("Video Muted");
+	}
+	else{
+		video.muted=false;
+		document.querySelector("#mute").innerHTML="Mute";
+		console.log("Video Unmuted");
+	}
+});
+
+document.querySelector("#volumeSlider").addEventListener("change", function() {
+	console.log("Volume Changed");
+	video.volume=this.value/100;
+	document.querySelector("#volume").innerHTML=video.volume*100+"%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("Old School");
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("Original");
+	video.classList.remove("oldSchool");
+});
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
